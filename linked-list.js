68855a1386b8e6a5a38eecf492561a74107c6968
B1t-Master <isaac.njuguna@strathmore.edu>
2025-11-27 13:27:45 +0300

    Add append function to linked-list implementaion

--- linked-list.js
+++ linked-list.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor(data) {
-    this.data = data;
+  constructor(value) {
+    this.value = value;
     this.next = null;
   }
 }
@@ -9,7 +9,52 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
-  append() {
-    let newNode = new Node(data);
+  append(value) {
+    let newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      return (current.next = newNode);
+    }
+  }
+  prepend() {}
+
+  size() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains() {}
+
+  find() {}
+  toString() {
+    let result = "";
+    let current = this.head;
+
+    while (current) {
+      result += `${current.value}` + "->";
+      current = current.next;
+    }
+    result += "null";
+    return result;
   }
 }
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
